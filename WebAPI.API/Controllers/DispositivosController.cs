37768bcd98c31c552a10459fcbf0e9d8c979a98e
Franciscosanchezavellan514 <franciscosanchezavellan514@gmail.com>
2024-11-30 14:16:53 -0600

    add  [Authorize]

--- WebAPI.API/Controllers/DispositivosController.cs
+++ WebAPI.API/Controllers/DispositivosController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 
 namespace WebAPI.API.Controllers
 {
+     [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class DispositivosController : ControllerBase
